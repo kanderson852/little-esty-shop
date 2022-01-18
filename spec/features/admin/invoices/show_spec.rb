6256a5c82fe17d7c4461f0d94bfee4f627ba9c3c
Kelly Anderson <kean9528@colorado.edu>
2022-01-18 13:53:56 -0700

    add discounts to admin page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -13,16 +13,18 @@ RSpec.describe 'Admin Invoices Show' do
       expect(page).to have_content(@invoice_1.customer.last_name)
     end
 
-    it 'I see the total revenue that will be generated from this invoice' do
-      visit "/admin/invoices/#{@invoice_1.id}"
-
-      expect(page).to have_content("Total revenue before discounts: #{@invoice_1.total_revenue}")
-    end
-
-    xit 'I see the total discounted revenue for my merchant from this invoice' do
-      visit "/admin/invoices/#{@invoice_1.id}"
-      expect(page).to have_content("Total Revenue with discounts")
-      expect(page).to have_content('')
+    it 'I see the total discounted revenue for my merchant from this invoice with and without discounts' do
+      merchant_a = Merchant.create!(name: "Kelly")
+      bulk_a = merchant_a.bulk_discounts.create!(percent: 20, threshhold: 10)
+      item_a = merchant_a.items.create!(name: "Item_a", description: "Description_a", unit_price: 1600)
+      item_b = merchant_a.items.create!(name: "Item_b", description: "Description_b", unit_price: 2300)
+      customer_a = Customer.create!(first_name: "Customera", last_name: "1a")
+      invoice_a = customer_a.invoices.create!
+      ii_a = invoice_a.invoice_items.create!(item_id: item_a.id, quantity: 10, unit_price: item_a.unit_price, status: 0)
+      ii_b = invoice_a.invoice_items.create!(item_id: item_b.id, quantity: 15, unit_price: item_b.unit_price, status: 0)
+      visit "/admin/invoices/#{invoice_a.id}"
+      expect(page).to have_content("Total revenue before discounts: $505")
+      expect(page).to have_content("Total Revenue with discounts: $404")
     end
 
     it 'I can update the invoice status' do
